c79779aeafbdc46fd423a5be9e913792d32f61b9
Luke Lazzaro <makorenwastaken@gmail.com>
2021-01-01 11:07:57 +1100

    Correctly get data and print it to show that it works

---
+++ pokeapi-viewer/DataViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DataViewController.swift
+//  pokeapi-viewer
+//
+//  Created by Luke Lazzaro on 1/1/21.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
--- pokeapi-viewer/ViewController.swift
+++ pokeapi-viewer/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    var responseData: Data?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func searchButtonPressed(_ sender: UIButton) {
+        var urlString = "https://pokeapi.co/api/v2/pokemon/"
+        
+        guard let text = textField.text else { return }
+        urlString.append(text)
+        
+        let url = URL(string: urlString)!
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            guard let data = data else {
+                fatalError("Error: \(error?.localizedDescription ?? "No error provided")")
+            }
+            
+            self.responseData = data
+            
+            DispatchQueue.main.async {
+                self.performSegue(withIdentifier: "dataViewer", sender: nil)
+            }
+        }
+        
+        task.resume()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let dvc = segue.destination as? DataViewController {
+            // pass properties to this controller
+            if let data = responseData {
+                print(String(data: data, encoding: .utf8)!)
+            }
+        }
+    }
+    
 }
 
